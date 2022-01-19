c6f779efc9ce15fe598accc2620fa5b453f95213
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-01-19 18:27:45 -0300

    código mais semântico e limpo para fácil vizualização do que foi feito

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function calculate() {
-    var valueinprimary = document.getElementById("value-select-primary")
-            if (valueinprimary.value.length == 0) {
+    var primaryValueSelect = document.getElementById("primary-value-select")
+            if (primaryValueSelect.value.length == 0) {
                 alert("Número não obtido!")
             } else {
-                var valueprimary = document.getElementById("select-primary").value
-                var valuesecond = document.getElementById("select-second").value
-                var Resultado = document.getElementById("result")
+                var primarySelect = document.getElementById("primary-select").value
+                var secondarySelect = document.getElementById("secondary-select").value
+                var result = document.getElementById("result-container-currency")
 
                 /* manipulação da section resultado */
-                var imgprimary = document.querySelector(".img-result-primary")
-                var textprimary = document.querySelector(".text-result-primary")
-                var valorprimary = document.querySelector(".value-result-primary")
-                var imgsecond = document.querySelector(".img-result-second")
-                var textsecond = document.querySelector(".text-result-second")
-                var valorsecond = document.querySelector(".value-result-second")
+                var primaryImg = document.querySelector(".primary-result-img")
+                var primaryTxt = document.querySelector(".primary-result-txt")
+                var primaryValue = document.querySelector(".primary-result-value")
+                var secondaryImg = document.querySelector(".secondary-result-img")
+                var secondaryTxt = document.querySelector(".secondary-result-txt")
+                var secondaryValue = document.querySelector(".secondary-result-value")
                 
                 /* valores guardados em object/array */
                 var valuecoin = {
@@ -22,48 +22,48 @@ function calculate() {
                     euro: [1.16,6.30],
                     format: function formatar(num) {return num * this.valuefirst }
                 }
-                valuecoin.valuefirst = valueinprimary.value
-                switch(valueprimary) {
+                valuecoin.valuefirst = primaryValueSelect.value
+                switch(primarySelect) {
                     case 'real': 
-                        if (valuesecond == 'dolar') {
-                            real(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                            dolar(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.real[0]).toLocaleString('en-us',{style: 'currency', currency: 'USD'})
+                        if (secondarySelect == 'dolar') {
+                            real(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                            dolar(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.real[0]).toLocaleString('en-us',{style: 'currency', currency: 'USD'})
                         } else {    
-                            real(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                            euro(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.real[1]).toLocaleString('es',{style: 'currency', currency: 'EUR'})
+                            real(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                            euro(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.real[1]).toLocaleString('es',{style: 'currency', currency: 'EUR'})
                         } 
                         break
                     case 'dolar': 
-                         if (valuesecond == 'real') {  
-                            dolar(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                            real(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.dolar[0]).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+                         if (secondarySelect == 'real') {  
+                            dolar(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                            real(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.dolar[0]).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
                          } else {
-                            dolar(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                            euro(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.dolar[1]).toLocaleString('es',{style: 'currency', currency: 'EUR'})
+                            dolar(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                            euro(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.dolar[1]).toLocaleString('es',{style: 'currency', currency: 'EUR'})
                          }
                         break
                     case 'euro':
-                        if (valuesecond == 'dolar') {
-                           euro(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                           dolar(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.euro[0]).toLocaleString('en-us',{style: 'currency', currency: 'USD'})
+                        if (secondarySelect == 'dolar') {
+                           euro(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                           dolar(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.euro[0]).toLocaleString('en-us',{style: 'currency', currency: 'USD'})
                         } else {
-                            euro(imgprimary, textprimary)
-                            valorprimary.textContent = valuecoin.valuefirst
-                            real(imgsecond, textsecond)
-                            valorsecond.textContent = valuecoin.format(valuecoin.euro[1]).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+                            euro(primaryImg, primaryTxt)
+                            primaryValue.textContent = valuecoin.valuefirst
+                            real(secondaryImg, secondaryTxt)
+                            secondaryValue.textContent = valuecoin.format(valuecoin.euro[1]).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
                         }
             }
-            Resultado.style.display = 'block' 
+            result.style.display = 'block' 
     }
 }
 
